a3d2c0fb1e06a5a7f0ee5cfd5583bf3dd97540d9
tamirbahalul <117384271+tamirbahalul@users.noreply.github.com>
2023-05-10 20:20:23 +0300

    Add files via upload

---
+++ html/loginPage.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Btravel</title>
+    <link rel="stylesheet" type="text/css" href="../css/style3.css">
+    <link rel="icon" href="../img/istockphoto-840458514-612x612.png">
+</head>
+
+<body>
+    <div id="navbar">
+        <div id="maindiv">
+            <div id="header">
+                <h1>Login To Your Account</h1>
+            </div>
+            <div id="downHeaderDiv">
+                <div class="login2">
+                    <form>
+                        <input type="text" placeholder="Email Address">
+                        <input type="password" placeholder="Password">
+                        <button id="cac">Login</button>
+                    </form>
+                </div>
+                <div id="login3">
+                    <p>
+                        Forget Password?<a href="#">Click Here</a>
+                    </p>
+                </div>
+            </div>   
+        </div>
+        <div id="logo">
+            <h1 class="wtext">Btravel</h1>
+            <p class="wtext">find your best attraction in israel</p>
+            <img id="flagimg" src="../img/israel.jpg">
+        </div>    
+    </div>
+    
+
+
+</body>
+
+</html>
---
+++ html/resetPassPage2.html
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Btravel</title>
+    <link rel="stylesheet" type="text/css" href="../css/style5.css">
+    <link rel="icon" href="../img/istockphoto-840458514-612x612.png">
+</head>
+
+<body>
+    <div id="navbar">
+        <div id="maindiv">
+            <div id="header">
+                <h1>Reset Password</h1>
+            </div>
+            <div id="downHeaderDiv">
+                <div class="login2">
+                    <form>
+                        <input type="password" placeholder="New Password">
+                        <input type="password" placeholder="Confirm Password">
+                        <button id="cac">Done</button>
+                    </form>
+                </div>
+            </div>   
+        </div>
+        <div id="logo">
+            <h1 class="wtext">Btravel</h1>
+            <p class="wtext">find your best attraction in israel</p>
+            <img id="flagimg" src="../img/israel.jpg">
+        </div>    
+    </div>
+    
+
+
+</body>
+
+</html>
